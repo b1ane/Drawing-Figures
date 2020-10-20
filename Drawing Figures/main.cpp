a12ad838f4352613e50d34e7a3e8c8f2ffc92c8b
Blane Zewdie <b1ane@outlook.com>
2020-10-19 23:19:23 -0500

    Initial Commit

---
+++ Drawing Figures/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Drawing Figures
+//
+//  Created by Blane Zewdie on 10/19/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
